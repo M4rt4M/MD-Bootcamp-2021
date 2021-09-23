fc88b02d7d72d51358686d96fc01ae151932161f
Marta <martmajew@gmail.com>
2021-09-23 14:53:37 +0100

    notes on js data types

--- session-2/challenges/main.js
+++ session-2/challenges/main.js
@@ -20,11 +20,31 @@ firstName = "Bob"; // vars declared with let have block scope
 console.log(firstName); // and can be changed
 
 const piePrice = 2.80; // also block-scoped
-piePrice = 1.99; // BUT throws error - cannot be edited
+// piePrice = 1.99; // BUT throws error - cannot be edited
 
 // Naming conventions
 let lastName; // camel-case
 
 // make sure the names are informative
 
-// variables can start with letter, $, or underscore
+// variable names can start with letter, $, or underscore
+
+// ------ DATA TYPES ------
+
+// Number (whole numbers, positive and negative)
+
+let noOfSubs = 100_000_000; // easy to write big numbers with underscore in between
+console.log(noOfSubs);
+
+// String
+let note = "This is a string";
+let anotherNote = `Backticks are the ES6 feature.
+It copies the string structure as it is.
+You can also reference variables easily using this method: ${note}`;
+
+console.log(anotherNote);
+
+// Array
+
+let names = ['John', 'Lauren', 'Art'];
+let mixedArray = [13, 'hello', 0.76];
