2cb1f6282309b332294cfb42db8c020cfa412ca9
Marta <martmajew@gmail.com>
2021-09-23 13:56:30 +0100

    DOM control cont

--- session-2/challenges/main.js
+++ session-2/challenges/main.js
@@ -3,3 +3,4 @@ console.error("This went wrong!");
 console.warn("This may go wrong");
 
 document.getElementById("dateNow").innerHTML = new Date();  //refers to the webpage (Document Object Model)
+document.getElementById("info").innerHTML = "Here is where info can go and you can dynamically edit it using js";
