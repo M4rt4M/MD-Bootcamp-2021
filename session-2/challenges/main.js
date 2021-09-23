c5377700d271cf21b1ea827727153fdde6cd8348
Marta <martmajew@gmail.com>
2021-09-23 13:51:03 +0100

    manipulating the DOM

--- session-2/challenges/main.js
+++ session-2/challenges/main.js
@@ -1,1 +1,5 @@
 console.log("hello world!");
+console.error("This went wrong!");
+console.warn("This may go wrong");
+
+document.getElementById("dateNow").innerHTML = new Date();  //refers to the webpage (Document Object Model)
