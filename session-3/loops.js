21891be526a5153bf7ac17a91fe4cc66fdd854c1
Marta <martmajew@gmail.com>
2021-09-27 14:20:31 +0100

    challenge setup

---
+++ session-3/loops.js
@@ -0,0 +1,15 @@
+// for loop
+/* for (start value - best to init with let; 
+        do loop while this is true for start var;
+        change to start value) */
+
+for (let i = 0; i <10; i+=2) {
+    console.log('i = ', i);
+};
+
+const numbers = [1, 3, 7, 12];
+
+for (let i = 0; i <numbers.length; i++) {
+    const doubled = numbers[i]*2;
+    console.log(doubled);
+};
