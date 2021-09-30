0488e7b12ff9e3f01b8b35fda0efd4c013efeac4
Marta Majewska <47524327+M4rt4M@users.noreply.github.com>
2021-09-30 09:42:55 +0100

    Update loops.js

--- session-3/loops.js
+++ session-3/loops.js
@@ -14,6 +14,33 @@ for (let i = 0; i <numbers.length; i++) {
     console.log(doubled);
 };
 
+// Ternary:
+const amount = 100;
+const shouldChargeTax = amount > 50 ? true : false;
+
+// Switch Statements:
+const coinInserted = 140;
+
+switch (coinInserted) {
+  case 20:
+  case 50:
+  case 100:
+    console.log(`Inserted ${coinInserted}`);
+    break;
+  default:
+    console.log("This machine only accepts 20p, 50p and £1");
+    break;
+};
+
+const num = 2;
+const classSize = 5;
+
+switch (classSize) {
+  case num < 3:
+    console.log("Works");
+    break;
+};
+
 // while loop
 
 // array function forEach
