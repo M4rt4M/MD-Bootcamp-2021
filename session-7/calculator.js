eebf9a9cd262b9bc32bfd1244b1a120e2277f361
Marta <martmajew@gmail.com>
2021-10-05 13:52:24 +0100

    introduction to modules in node

--- session-7/calculator.js
+++ session-7/calculator.js
@@ -4,11 +4,19 @@ function add(...nums) {
     for (i = 0; i < nums.length; i++) {
         total +=nums[i];
     }
-}
+
+    return total;
+};
 
 function divide(num1, num2) {
-    if (num2=0) {
+    if (num2===0) {
         throw new Error("You cannot divide by zero!");
     }
     return num1/num2;
-}
+};
+
+console.log(divide(3,1));
+
+module.exports = 
+{add,
+divide}
---
+++ session-7/mainCalc.js
@@ -0,0 +1,3 @@
+const {add, divide} = require("c:/Users/Marta/Documents/Bootcamp21/session-7/calculator");
+console.log("Add using require ", add(1, 4, 6));
+console.log("Divide using require ", divide(6, 2));
