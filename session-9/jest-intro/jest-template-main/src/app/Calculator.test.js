f3da6b85f27d575fb3fab3918078c3f1bc373712
Marta <martmajew@gmail.com>
2021-10-07 11:05:20 +0100

    intro to testing with jest

---
+++ session-9/jest-intro/jest-template-main/src/app/Calculator.test.js
@@ -0,0 +1,47 @@
+import {addAllNumbers, divide} from "./Calculator";
+// ^best parctice to use relative path
+
+// Group multiple individual tests into describe() block
+describe("Calculator: ", () => {
+    it("Should add all numbers", () => {
+    /* AAA:
+    Arrange (optional - example data, omit if function does not take any args)
+    Act
+    Assert*/
+
+    // Arrange
+        const arr = [1, 2, 3];
+
+    // Act
+        const result = addAllNumbers(arr);
+    
+    // Assert
+        expect(result).toBe(6);
+
+})
+// ^anonymous function is used here becuase it's only needed for this one test
+
+    it("Should divide two numbers", () => {
+        const num1 = 10;
+        const num2 = 2;
+
+        const result = divide(num1, num2);
+
+        expect(result).toBe(5);
+    });
+
+    // You can organise tests further into smaller blocks
+    describe("Edge Cases:", () => {
+        it("Should not add text when adding numbers", () => {
+            // Arrange
+            const arr = [1, 2, 3, "hello", "5"];
+
+            // Act
+            const result = addAllNumbers(arr);
+        
+            // Assert
+            expect(result).toBe(6);
+        })
+    })
+});
+
