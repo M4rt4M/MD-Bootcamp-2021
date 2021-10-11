9349f35efb4b865200b1fc3d6e5c72f3b7554d83
Marta <martmajew@gmail.com>
2021-10-11 13:48:45 +0100

    intro to REGEX

---
+++ session-11/main.js
@@ -0,0 +1,18 @@
+// REGEX
+
+// conventianally declared/initialised as const
+const messageRE = /hello/;
+const actualMessage = "hello";
+
+//verifies if there is a match to a re and teturns a Boolean
+if (messageRE.test(actualMessage)) {
+    console.log("this matched", actualMessage)
+};
+
+const atRE = /at/g; //global tag
+const phrase = `
+The fat cat ran down the street.
+It was searching for a mouse to eat.
+`;
+
+console.log(atRE.exec(phrase)); //search globally for the match
