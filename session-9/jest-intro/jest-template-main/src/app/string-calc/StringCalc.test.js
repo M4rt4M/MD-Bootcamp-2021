f34db062971f42aa1098a45a1b34388ab6b1bc9a
Marta <martmajew@gmail.com>
2021-10-11 16:32:27 +0100

    cleanup and string calc start

---
+++ session-9/jest-intro/jest-template-main/src/app/string-calc/StringCalc.test.js
@@ -0,0 +1,10 @@
+import { calculator } from "./StringCalc.js";
+
+describe("StringCalculator", () => {
+
+    it("should return 0 if given empty string", () => {
+        const result = calculator("");
+        expect(result).toBe(0)
+    });
+
+});
